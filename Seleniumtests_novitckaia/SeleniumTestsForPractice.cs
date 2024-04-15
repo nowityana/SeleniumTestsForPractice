657778c63ed463ee0a55a6a98731dae5c9ae9f65
nowityana <nowityana@gmail.com>
2024-04-15 14:11:59 +0300

    Тест для подготовки к практике.

---
+++ Seleniumtests_novitckaia/SeleniumTestsForPractice.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Seleniumtests_novitckaia;
+
+public class SeleniumTestsForPractice
+{
+    [Test]
+    public void Authorization()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        
+        // зайти в хром (с помощью вебдрайвера)
+        var driver = new ChromeDriver(options);
+        
+        // перейти по урлу https://staff-testing.testkontur.ru
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        Thread.Sleep(5000);
+        
+        // ввести логин и пароль
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("user");
+
+        var password = driver.FindElement(By.Name("Password"));
+        password.SendKeys("1q2w3e4r%T");
+        
+        Thread.Sleep(5000);
+
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
+        
+        Thread.Sleep(5000);
+        
+        // проверяем, что мы находимся на нужной странице
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
+        
+        // закрываем браузер и убиваем процесс драйвера
+        driver.Quit();
+    }
+}
