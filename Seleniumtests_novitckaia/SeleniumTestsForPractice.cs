d780ebb9bbae96388674b09a3b6d55fa3f15b899
nowityana <nowityana@gmail.com>
2024-04-20 15:56:38 +0300

    Авторизация в отдельном методе.
    Настройки браузера и авторизация вызываются в Setup.
    Добавлен метод вызова бокового меню.

--- Seleniumtests_novitckaia/SeleniumTestsForPractice.cs
+++ Seleniumtests_novitckaia/SeleniumTestsForPractice.cs
@@ -1,51 +1,71 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using SeleniumExtras.WaitHelpers;
 using FluentAssertions;
+using System.Drawing;
 
 namespace Seleniumtests_novitckaia;
 
 public class SeleniumTestsForPractice
 {
     public ChromeDriver driver;
-    
-    [Test]
-    public void Authorization()
+
+    [SetUp]
+    public void Setup()
     {
         var options = new ChromeOptions();
         options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
         
-        // зайти в хром (с помощью вебдрайвера)
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options); // зайти в хром (с помощью вебдрайвера)
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2); // неявное ожидание
         
-        // перейти по урлу https://staff-testing.testkontur.ru
-        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
-        
-        // ввести логин и пароль
-        var login = driver.FindElement(By.Id("Username"));
-        login.SendKeys("user");
-
-        var password = driver.FindElement(By.Name("Password"));
-        password.SendKeys("1q2w3e4r%T");
-        
-        // нажать на кнопку "Войти"
-        var enter = driver.FindElement(By.Name("button"));
-        enter.Click();
-
+        Autorization(); // авторизация
+    }
+    
+    [Test]
+    public void AutorizationTest() // тест на авторизацию
+    {
         var news = driver.FindElement(By.CssSelector("[data-tid='Title']"));
         
-        // проверяем, что мы находимся на нужной странице
-        var currentUrl = driver.Url;
-        currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
+        var currentUrl = driver.Url; 
+        currentUrl.Should().Be("https://staff-testing.testkontur.ru/news"); // проверяем урл
+    }
+
+    [Test]
+    public void SideBarMenuTest() // тест на вызов бокового меню и переход из него на страницу Сообщества 
+    {
+        // установка маленького размера,чтобы у экранов с большим разрешением не падал тест из-за отсуствия бокового меню
+        driver.Manage().Window.Size = new Size(800, 600); 
+        var sideMenu = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']")); 
+        sideMenu.Click(); // нажать на боковое меню
+        
+        var community = driver.FindElements(By.CssSelector("[data-tid='Community']")).First(element => element.Displayed);
+        community.Click(); // нажать на "Сообщества"
+        
+        // проверяем что сообщества есть на странице
+        var communityTitle = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']")); 
+        driver.Url.Should().Be("https://staff-testing.testkontur.ru/communities"); // проверяем урл
+    }
+    
+    public void Autorization() // метод авторизации
+    {
+        // перейти по урлу https://staff-testing.testkontur.ru
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru"); 
+        
+        var login = driver.FindElement(By.Id("Username")); // ввести логин
+        login.SendKeys("novitsckaja.yana@yandex.ru");
+
+        var password = driver.FindElement(By.Name("Password")); // ввести пароль
+        password.SendKeys("Lnfffjd5fd3kdf!");
+        
+        var enter = driver.FindElement(By.Name("button")); // нажать на кнопку "Войти"
+        enter.Click();
     }
     
     [TearDown]
     public void TearDown()
     {
-        // закрываем браузер и убиваем процесс драйвера
-        driver.Quit(); 
+        driver.Close(); // закрываем браузер
+        driver.Quit(); // убиваем процесс драйвера
     }
 }
